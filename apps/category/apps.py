0707cbc248dc12b8278dac9e1bb4654dc800cd02
Julio Patiño <franjp132@gmail.com>
2023-09-14 14:17:26 -0500

    configure apps

--- apps/category/apps.py
+++ apps/category/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.category'
+    name = 'Advanced_Blog.apps.category'
